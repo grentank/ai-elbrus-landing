adb5871f8006c56f70c8c49d94479653130f30af
grentank <mexmat100394@gmail.com>
2025-04-25 14:47:56 +0300

    closed 3rd

--- components/payment-section.tsx
+++ components/payment-section.tsx
@@ -184,16 +184,16 @@ export default function PaymentSection({
               <p className="font-medium text-base text-gray-700 mb-2">
                 Ближайшие потоки:
               </p>
-              {/* <div className="flex items-center gap-2 bg-gray-50 p-3 rounded-md opacity-50">
+              <div className="flex items-center gap-2 bg-gray-50 p-3 rounded-md opacity-50">
                 <Calendar className="h-4 w-4 text-gray-400" />
                 <span className="line-through">
-                  2-й поток — старт 14 апреля
+                  3-й поток — старт 28 апреля
                 </span>
                 <span className="ml-auto text-xs bg-gray-100 text-gray-600 px-2 py-1 rounded">
                   набор закрыт
                 </span>
-              </div> */}
-              <div className="flex items-center gap-2 bg-white p-3 rounded-md border-2 border-indigo-100">
+              </div>
+              {/* <div className="flex items-center gap-2 bg-white p-3 rounded-md border-2 border-indigo-100">
                 <Calendar className="h-4 w-4 text-indigo-600" />
                 <span className="font-medium">3-й поток — старт 28 апреля</span>
                 <span className="ml-auto text-xs bg-yellow-100 text-yellow-800 px-2 py-1 rounded">
@@ -203,19 +203,30 @@ export default function PaymentSection({
               <div className="flex items-center gap-1 mt-2">
                 <Clock className="h-4 w-4" />
                 <span>Длительность: 2 недели, каникулы с 5 по 11 мая</span>
-              </div>
+              </div> */}
 
               <div className="flex items-center gap-2 bg-white p-3 rounded-md border-2 border-indigo-100">
                 <Calendar className="h-4 w-4 text-indigo-600" />
                 <span className="font-medium">4-й поток — старт 19 мая</span>
                 <span className="ml-auto text-xs bg-yellow-100 text-yellow-800 px-2 py-1 rounded">
-                  осталось 14 мест
+                  осталось 11 мест
                 </span>
               </div>
               <div className="flex items-center gap-1 mt-2">
                 <Clock className="h-4 w-4" />
                 <span>Длительность: 2 недели</span>
               </div>
+              {/* <div className="flex items-center gap-2 bg-white p-3 rounded-md border-2 border-indigo-100">
+                <Calendar className="h-4 w-4 text-indigo-600" />
+                <span className="font-medium">5-й поток — старт 2 июня</span>
+                <span className="ml-auto text-xs bg-yellow-100 text-yellow-800 px-2 py-1 rounded">
+                  осталось 11 мест
+                </span>
+              </div>
+              <div className="flex items-center gap-1 mt-2">
+                <Clock className="h-4 w-4" />
+                <span>Длительность: 2 недели</span>
+              </div>*/}
             </div>
 
             <div className="mt-auto">
